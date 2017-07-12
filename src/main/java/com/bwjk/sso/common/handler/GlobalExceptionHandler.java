a1ea011a52c3699f956218af0b0613ba720bdbd0
zhangxlly <zhangxlly@163.com>
2017-07-12 17:48:02 +0800

    Controlleradvice测试类提交

---
+++ src/main/java/com/bwjk/sso/common/handler/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.bwjk.sso.common.handler;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by zxl on 2017/7/12.
+ */
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger LOGGER = Logger.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    public void testException(HttpServletRequest request, Exception ex) {
+        System.out.println("--Exception回调开始--");
+        LOGGER.info("Exception:" + ex.getMessage());
+        System.out.println(ex.getMessage());
+        System.out.println("--Exception回调结束--");
+    }
+}
